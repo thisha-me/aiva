dd2ec07de078c948452dea7e82f06cac08e9f9de
thisha-me <d.thishakya@gmail.com>
2025-12-27 17:38:28 +0530

    restructure: move media to src/aiva-face and convert to TypeScript

--- src/extension.ts
+++ src/extension.ts
@@ -102,18 +102,18 @@ class AivaViewProvider implements vscode.WebviewViewProvider {
 	private _getHtmlForWebview(webview: vscode.Webview) {
 		// Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
 		const scriptUri = webview.asWebviewUri(
-			vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
+			vscode.Uri.joinPath(this._extensionUri, "out", "aiva-face", "aiva.js")
 		);
 
 		// Do the same for the stylesheet.
 		const styleResetUri = webview.asWebviewUri(
-			vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+			vscode.Uri.joinPath(this._extensionUri, "out", "aiva-face", "reset.css")
 		);
 		const styleVSCodeUri = webview.asWebviewUri(
-			vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+			vscode.Uri.joinPath(this._extensionUri, "out", "aiva-face", "vscode.css")
 		);
 		const styleMainUri = webview.asWebviewUri(
-			vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
+			vscode.Uri.joinPath(this._extensionUri, "out", "aiva-face", "aiva.css")
 		);
 
 		// Use a nonce to only allow a specific script to be run.
